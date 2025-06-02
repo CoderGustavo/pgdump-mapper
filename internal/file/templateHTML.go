2c8700aecb62a8febf424380d952f10d1707b436
Hediberto C Silva <hed.cavalcante@gmail.com>
2025-06-01 23:13:37 -0300

    Rename Title HTML
    Signed-off-by: Hediberto C Silva <hed.cavalcante@gmail.com>

--- internal/file/templateHTML.go
+++ internal/file/templateHTML.go
@@ -4,7 +4,7 @@ const htmlTemplate = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Table Viewer</title>
+    <title>PGDump-Mapper</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script>
         function showTable() {
